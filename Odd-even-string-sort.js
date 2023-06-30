be1dfc2c8557a8ad7e68bd3b5f9071f8cbec4ec8
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-06-30 16:57:29 -0700

    Create Odd-even-string-sort.js

---
+++ Odd-even-string-sort.js
@@ -0,0 +1,11 @@
+/*
+Given a string s, you have to return another string 
+such that even-indexed and odd-indexed characters of s 
+are grouped and groups are space-separated.
+
+Note: 
+0 is considered to be an even index. 
+All input strings are valid with no spaces.
+*/
+
+
